095030877828addc78928d979dcaef03edb62cf7
Youssouf SYLLA <youssoufs1209@gmail.com>
2025-09-28 21:56:37 +0000

    Resolution du bug 3h apres

--- employe/views.py
+++ employe/views.py
@@ -5,4 +5,4 @@ from .models import Employe
 
 def list_emloyes(request):
     employes = Employe.objects.all()
-    return render(request, 'employe/list_employes.html', {'employes': employes})
+    return render(request, 'employe/list.html', {'employes': employes})
